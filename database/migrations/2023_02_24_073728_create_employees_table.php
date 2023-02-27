e05688f577f1619be83653edace27e88f6c05181
info <info@dynamicdreamz.com>
2023-02-27 14:58:36 +0530

    bootstrap config database added

---
+++ database/migrations/2023_02_24_073728_create_employees_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id('empid');
+            $table->string('empname');
+            $table->string('designation');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->text('empimg');
+            $table->string('gender');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
